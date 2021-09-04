8127a5cd951a6127074cb61de89de2ef09ee8c03
Soumya  HV <soumyahv6@gmail.com>
2021-09-04 18:33:49 +0530

    'integrated auth but styles and routing to b added'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from '../app/shared/shared.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import firebase from 'firebase/app'; 
+import { environment } from 'src/environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire'
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule, 
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule, ReactiveFormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,14 +1,48 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 import * as env from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    constructor(private http: HttpClient) {}
+
+    confirmationResult: firebase.auth.ConfirmationResult;
+
+    constructor(private http: HttpClient, private fireAuth: AngularFireAuth) { }
 
     registerEvent(body) {
-       return this.http.post(env.environment.url + 'login', body );
+        return this.http.post(env.environment.url + 'login', body);
+    }
+
+
+    public signInWithPhoneNumber(recaptchaVerifier, phoneNumber) {
+        console.log(recaptchaVerifier, phoneNumber);
+        
+        return new Promise<any>((resolve, reject) => {
+
+            this.fireAuth.signInWithPhoneNumber(phoneNumber, recaptchaVerifier)
+                .then((confirmationResult) => {
+                    this.confirmationResult = confirmationResult;
+                    resolve(confirmationResult);
+                }).catch((error) => {
+                    console.log(error);
+                    reject('SMS not sent');
+                });
+        });
+    }
+    public async enterVerificationCode(code) {
+        return new Promise<any>((resolve, reject) => {
+            this.confirmationResult.confirm(code).then(async (result) => {
+                console.log(result);
+                const user = result.user;
+                resolve(user);
+            }).catch((error) => {
+                reject(error.message);
+            });
+
+        });
     }
 }
