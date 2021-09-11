fd72bd6f58d0126f03943f0389bcb20f0d9fabfc
Soumya  HV <soumyahv6@gmail.com>
2021-09-11 13:42:46 +0530

    'tab 3 design with sliding'

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { commonService } from '../core/services/common-service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  noOfProducts : number = 1;
+  stockLists = [{id: 1, image: '../../assets/images/chilli.svg', pName: 'Green Chilli', qty: '100g', price: 80, qtyno:1},
+  {id: 2, image: '../../assets/images/tomato.svg', pName: 'Tomato', qty: '1kg', price:100, qtyno:1}];
+  constructor(private router:Router, private tabService: commonService) { }
 
-  constructor() {}
+  ngOnInit() {}
 
+  reduceNumber() {
+    if(this.noOfProducts > 0) {
+      this.noOfProducts -= 1;
+    }
+  }
+
+  increaseNumber() {
+    this.noOfProducts += 1;
+  }
+
+  backHome(){
+    this.router.navigate(['tab/tab2']);
+    this.tabService.cartScreen = false;
+  }
+
+  displayImg() {
+
+  }
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../core/services/auth.service';
+import { commonService } from '../core/services/common-service';
 
 @Component({
   selector: 'app-tabs',
@@ -7,18 +9,18 @@ import { Router } from '@angular/router';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-  cartScreen = false;
-  constructor(private router: Router) {
-   
+  constructor(private router: Router, private tabservice: commonService) {
+   console.log(this.router.url);
+   this.tabservice.cartScreen = (this.router.url == '/tab/tab3') ? true : false
   }
 
   cartListClicked() {
-      this.cartScreen = true;
-      this.router.navigate(['tab/tab4']);
+      this.tabservice.cartScreen = true;
+      this.router.navigate(['tab/tab3']);
   }
 
   otherTabClicked() {
-    this.cartScreen = false;
+    this.tabservice.cartScreen = false;
   }
 
 }
