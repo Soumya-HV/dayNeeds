35e7597bde3ce82b3543054830f219974223e438
pappuraj <pappugurusamy93@gmail.com>
2021-09-22 00:03:59 +0530

    updated

--- src/app/tab4/tab4.component.ts
+++ src/app/tab4/tab4.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+import { commonService } from '../core/services/common-service';
 
 @Component({
   selector: 'app-tab4',
@@ -8,8 +10,12 @@ import { ModalController } from '@ionic/angular';
 })
 export class Tab4Component implements OnInit {
   showDetails =false;
-  constructor(public modalController: ModalController) { }
+  constructor(public modalController: ModalController,private router: Router, private tabService: commonService) { }
 
   ngOnInit() { }
 
+  backHome() {
+    this.router.navigate(['tab/tab2']);
+    
+  }
 }
