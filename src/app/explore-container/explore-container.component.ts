5abb2bf65dbdf4083a09954956a2d188caec9059
pappuraj <pappugurusamy93@gmail.com>
2021-09-21 23:52:22 +0530

    myorders updated

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./explore-container.component.scss'],
 })
 export class ExploreContainerComponent implements OnInit {
+  noOfProducts: number = 1;
+  tabSelectedModel = 'orderplaced';
+  orderDetails = [{ id: 1, apartmentName: 'Skandha', deliveryTime: 'Today 4:00pm', products: [{ houseNo: 1, blockNo: 'A', prodName: 'Nandini Special Toned Milk', qty: '1000ml' }] },
+  { id: 2, apartmentName: 'Home Garden', deliveryTime: 'Today 6:00pm', products: [{ houseNo: 1, blockNo: 'C', prodName: 'Nandini Milk', qty: '500ml' }] }]
   @Input() name: string;
 
   constructor() { }
--- src/app/tab4/tab4.component.ts
+++ src/app/tab4/tab4.component.ts
@@ -7,30 +7,9 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./tab4.component.scss'],
 })
 export class Tab4Component implements OnInit {
-  noOfProducts: number = 1;
-  tabSelectedModel = 'orderplaced';
-  orderDetails = [{ id: 1, apartmentName: 'Skandha', deliveryTime: 'Today 4:00pm', products: [{ houseNo: 1, blockNo: 'A', prodName: 'Nandini Special Toned Milk', qty: '1000ml' }] },
-  { id: 2, apartmentName: 'Home Garden', deliveryTime: 'Today 6:00pm', products: [{ houseNo: 1, blockNo: 'C', prodName: 'Nandini Milk', qty: '500ml' }] }]
+  showDetails =false;
   constructor(public modalController: ModalController) { }
 
   ngOnInit() { }
 
-  reduceNumber() {
-    if (this.noOfProducts > 0) {
-      this.noOfProducts -= 1;
-    }
-  }
-
-  increaseNumber() {
-    this.noOfProducts += 1;
-  }
-
-
-  closeModal() {
-    this.modalController.dismiss();
-  }
-
-  segmentChanged(ev: any) {
-    console.log('Segment changed', ev.detail, this.tabSelectedModel);
-  }
 }
