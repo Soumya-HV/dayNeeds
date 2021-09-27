3926edcc6a5957fc7aa3667cbed51abef702c4c7
pappuraj <pappugurusamy93@gmail.com>
2021-09-27 21:28:19 +0530

    updated code

--- src/app/vendor/vendor-orders/vendor-orders.page.ts
+++ src/app/vendor/vendor-orders/vendor-orders.page.ts
@@ -6,7 +6,119 @@ import { Component } from '@angular/core';
   styleUrls: ['vendor-orders.page.scss']
 })
 export class VendorOrdersPage {
+  // segment = 'placed';
+  selectedRestaurantIdx: any;
+  orderDetails = [
+    {
+      "id": 1,
+      "apartmentName": "Skandha",
+      "deliveryTime": "Today 4:00pm",
+      "products": [
+        {
+          "segment": "placed",
+          "orderPlaced": [
+            {
+              "name": "user1",
+              "amount": "350",
+              "status": "cod"
+            },
+            {
+              "name": "user2",
+              "amount": "350",
+              "status": "paid"
+            }
+          ]
+        },
+        {
+          "segemnt": "ontheway",
+          "ontheway": [
+            {
+              "name": "user3",
+              "amount": "500",
+              "status": "cod"
+            },
+            {
+              "name": "user4",
+              "amount": "400",
+              "status": "paid"
+            }
+          ]
+        },
+        {
+          "segemnt": "delivered",
+          "delivered": [
+            {
+              "name": "user5",
+              "amount": "200",
+              "status": "paid"
+            },
+            {
+              "name": "user6",
+              "amount": "450",
+              "status": "paid"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 1,
+      "apartmentName": "Stivatsave",
+      "deliveryTime": "Today 5:00pm",
+      "products": [
+        {
+          "segment": "placed",
+          "orderPlaced": [
+            {
+              "name": "user1",
+              "amount": "350",
+              "status": "cod"
+            },
+            {
+              "name": "user2",
+              "amount": "350",
+              "status": "paid"
+            }
+          ]
+        },
+        {
+          "segemnt": "ontheway",
+          "ontheway": [
+            {
+              "name": "user3",
+              "amount": "500",
+              "status": "cod"
+            },
+            {
+              "name": "user4",
+              "amount": "400",
+              "status": "paid"
+            }
+          ]
+        },
+        {
+          "segemnt": "delivered",
+          "delivered": [
+            {
+              "name": "user5",
+              "amount": "200",
+              "status": "paid"
+            },
+            {
+              "name": "user6",
+              "amount": "450",
+              "status": "paid"
+            }
+          ]
+        }
+      ]
+    }
+  ]
 
-  constructor() {}
+  constructor() { }
+
+  changeSegment(index: number) {
+    this.selectedRestaurantIdx = index;
+  }
 
 }
