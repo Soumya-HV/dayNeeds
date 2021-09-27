15128a6613cefc4f10f9ff3179b3a1cfae05e25e
pappuraj <pappugurusamy93@gmail.com>
2021-09-27 09:22:27 +0530

    updated code

--- src/app/customer/customer-homepage/customer-homepage.ts
+++ src/app/customer/customer-homepage/customer-homepage.ts
@@ -5,6 +5,7 @@ import { commonService } from '../../core/services/common-service';
 import { SideMenuCustomerComponent } from '../../sidemenu-customer/sidemenu-customer.component';
 import * as env from '../../../environments/environment';
 import { NotificationComponent } from '../../shared/components/notification/notification.component';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-homepage',
@@ -12,28 +13,30 @@ import { NotificationComponent } from '../../shared/components/notification/noti
   styleUrls: ['customer-homepage.scss']
 })
 export class CustomerHomePage {
-  categories = [
-    {
-      'img': '../../assets/images/cat-1.svg',
-      'bgColor': '#DDEDEA',
-      'name': 'veggies'
-    },
-    {
-      'img': '../../assets/images/cat-2.svg',
-      'bgColor': '#FCF4DD',
-      'name': 'Fruits'
-    },
-    {
-      'img': '../../assets/images/cat-3.svg',
-      'bgColor': '#DAEAF6',
-      'name': 'Food'
-    },
-    {
-      'img': '../../assets/images/cat-4.svg',
-      'bgColor': '#FCE1E4',
-      'name': 'Meat'
-    },
-  ]
+  categories : any;
+  dabbastoriesLists: any;
+  // categories = [
+  //   {
+  //     'img': '../../assets/images/cat-1.svg',
+  //     'bgColor': '#DDEDEA',
+  //     'name': 'veggies'
+  //   },
+  //   {
+  //     'img': '../../assets/images/cat-2.svg',
+  //     'bgColor': '#FCF4DD',
+  //     'name': 'Fruits'
+  //   },
+  //   {
+  //     'img': '../../assets/images/cat-3.svg',
+  //     'bgColor': '#DAEAF6',
+  //     'name': 'Food'
+  //   },
+  //   {
+  //     'img': '../../assets/images/cat-4.svg',
+  //     'bgColor': '#FCE1E4',
+  //     'name': 'Meat'
+  //   },
+  // ]
   @ViewChild('mySlider') slider: IonSlides;
   @ViewChild('categorySlider') catslider: IonSlides;
   sliderOpts = {
@@ -47,8 +50,9 @@ export class CustomerHomePage {
     speed: 1000,
 
   }
-  constructor(public modalController: ModalController, private http: HttpClient, private cmnService: commonService) {
+  constructor(public modalController: ModalController, private http: HttpClient, private cmnService: commonService ,private router : Router) {
     this.getCategories();
+    this.getDabbaStoriesList();
   }
 
   async openSideModal(name) {
@@ -65,8 +69,29 @@ export class CustomerHomePage {
   getCategories() {
     this.http.get(env.environment.url + 'categories').subscribe
       (res => {
+        this.categories = res['response'];
+        console.log(this.categories);
       // console.log("category lists"+res);
       });
+  }
+
+
+
+  navigateToVendorList(category){
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+       "category_id":category._id
+      }
+    };
+   this.router.navigate(['customer/vendor-list/vendorfilterbyCatId'],navigationExtras); 
+  }
+
+  getDabbaStoriesList(){
+    this.http.get(env.environment.url + 'item/home-made-items').subscribe
+    (res => {
+      this.dabbastoriesLists = res['response'];
+      console.log(this.dabbastoriesLists);
+    });
   }
 
 }
